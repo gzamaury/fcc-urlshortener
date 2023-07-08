5d1421a427ece6b862180bd3f1b8c2d09ad3edf0
Amaury Gz <gzamaury@hotmail.com>
2023-07-08 08:10:45 +0000

    refactor: Mongoose upgrade

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.set("strictQuery", true).connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
