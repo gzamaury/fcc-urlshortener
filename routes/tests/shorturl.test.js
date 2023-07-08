40f049fb0d7a0e583ead89b9bf731cf426098dba
Amaury Gz <gzamaury@hotmail.com>
2023-07-08 07:57:45 +0000

    refactor: Modularizing tests

---
+++ routes/tests/shorturl.test.js
@@ -0,0 +1,46 @@
+const request = require("supertest");
+const app = require("../../index");
+const mongoose = require("mongoose");
+
+beforeAll(async () => {
+  // clear the test database
+  await mongoose.connection.dropDatabase();
+});
+
+const invalidURL = "https:/freecodecamp.org";
+const validUrl = "https://freecodecamp.org";
+
+describe("POST /api/shorturl", () => {
+  let short_url;
+
+  it("should return an error for invalid url", async () => {
+    const response = await request(app).post("/api/shorturl").send({
+      url: invalidURL,
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("error", "invalid url");
+  });
+
+  it("should return a JSON object with original_url and short_url", async () => {
+    const response = await request(app)
+      .post("/api/shorturl")
+      .send({ url: validUrl });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("original_url", validUrl);
+    expect(response.body).toHaveProperty("short_url");
+
+    short_url = response.body.short_url;
+  });
+
+  describe("GET /api/shorturl/:short_url", () => {
+    it("should redirect to the original url", async () => {
+      const response = await request(app)
+        .get(`/api/shorturl/${short_url}`)
+        .expect(302);
+
+      expect(response.headers.location).toBe(validUrl);
+    });
+  });
+});
--- tests/api.test.js
+++ tests/api.test.js
@@ -1,11 +1,5 @@
 const request = require("supertest");
 const app = require("../index");
-const mongoose = require("mongoose");
-
-beforeAll(async () => {
-  // clear the test database
-  await mongoose.connection.dropDatabase();
-});
 
 describe("GET /api/hello", () => {
   it("should return a JSON object with greeting", async () => {
@@ -13,43 +7,5 @@ describe("GET /api/hello", () => {
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual({ greeting: "hello API" });
-  });
-});
-
-const invalidURL = "https:/freecodecamp.org";
-const validUrl = "https://freecodecamp.org";
-
-describe("POST /api/shorturl", () => {
-  let short_url;
-
-  it("should return an error for invalid url", async () => {
-    const response = await request(app).post("/api/shorturl").send({
-      url: invalidURL,
-    });
-
-    expect(response.status).toBe(400);
-    expect(response.body).toHaveProperty("error", "invalid url");
-  });
-
-  it("should return a JSON object with original_url and short_url", async () => {
-    const response = await request(app)
-      .post("/api/shorturl")
-      .send({ url: validUrl });
-
-    expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty("original_url", validUrl);
-    expect(response.body).toHaveProperty("short_url");
-
-    short_url = response.body.short_url;
-  });
-
-  describe("GET /api/shorturl/:short_url", () => {
-    it("should redirect to the original url", async () => {
-      const response = await request(app)
-        .get(`/api/shorturl/${short_url}`)
-        .expect(302);
-
-      expect(response.headers.location).toBe(validUrl);
-    });
   });
 });
