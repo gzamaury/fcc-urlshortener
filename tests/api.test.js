459fd7cbd8ec4846140cb3f52bbe10ba2c942666
Amaury Gz <gzamaury@hotmail.com>
2023-07-08 05:34:52 +0000

    chore: Add tests for GET /api/shorturl/:short_url endpoint

--- tests/api.test.js
+++ tests/api.test.js
@@ -20,6 +20,8 @@ const invalidURL = "https:/freecodecamp.org";
 const validUrl = "https://freecodecamp.org";
 
 describe("POST /api/shorturl", () => {
+  let short_url;
+
   it("should return an error for invalid url", async () => {
     const response = await request(app).post("/api/shorturl").send({
       url: invalidURL,
@@ -38,5 +40,17 @@ describe("POST /api/shorturl", () => {
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty("original_url", validUrl);
     expect(response.body).toHaveProperty("short_url");
+
+    short_url = response.body.short_url;
+  });
+
+  describe("GET /api/shorturl/:short_url", () => {
+    it("should redirect to the original url", async () => {
+      const response = await request(app)
+        .get(`/api/shorturl/${short_url}`)
+        .expect(302);
+
+      expect(response.headers.location).toBe(validUrl);
+    });
   });
 });
